e96e67b777ef066ed318aa897eedd24d4a0dd52e
unknown <juliana_guonik@hotmail.com>
2022-04-19 21:10:26 -0300

    Criando API, conectando com o banco de dados

---
+++ models/Gender.js
@@ -0,0 +1,25 @@
+const db = require('../database');
+const {
+    DataTypes
+} = require('sequelize');
+
+
+const Gender = db.define('Gender', {
+    gender_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    gender: {
+        type: DataTypes.STRING,
+    },
+},
+    {
+        tableName:"gender",
+        timestamps: false,
+
+    },
+
+);
+
+module.exports = Gender;
